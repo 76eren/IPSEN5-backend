6d6363259081dd23a6d7f53a9296fdfe523b887d
Eren <e.basaran2004@gmail.com>
2024-04-22 14:12:57 +0200

    feat: project setup

---
+++ src/main/java/com/cgi/ipsen5/Ipsen5BackendApplication.java
@@ -0,0 +1,13 @@
+package com.cgi.ipsen5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ipsen5BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ipsen5BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cgi/ipsen5/Ipsen5BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgi.ipsen5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ipsen5BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
