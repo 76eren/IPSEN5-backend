78204983d0baf6db44ec02be97cb5160fc064c60
Stan Smits <s1136644@student.hsleiden.nl>
2024-05-15 09:18:35 +0200

    fix: change reserve endpoint to location

--- src/main/java/com/cgi/ipsen5/Controller/LocationController.java
+++ src/main/java/com/cgi/ipsen5/Controller/LocationController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/api/v1/reserve")
+@RequestMapping(value = "/api/v1/location")
 @RequiredArgsConstructor
 public class LocationController {
     private final LocationDao reserveDao;
