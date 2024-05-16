95693b22a8eca3a24c265e575d9ff2e936f76871
isabelhb <isabelbohre@gmail.com>
2024-05-16 14:09:33 +0200

    FEAT: unit test for email service

---
+++ src/test/java/com/cgi/ipsen5/MailSender/EmailServiceUnitTest.java
@@ -0,0 +1,46 @@
+package com.cgi.ipsen5.MailSender;
+
+import com.cgi.ipsen5.Service.ResetlinkEmailService;
+import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
+import net.minidev.json.parser.ParseException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class EmailServiceUnitTest {
+    ResetlinkEmailService mailService;
+
+    @BeforeEach
+    void setUp() {
+        mailService = new ResetlinkEmailService();
+        JSONParser parser = new JSONParser();
+
+        try {
+            Object obj = parser.parse(new FileReader("src/main/resources/credentials.json"));
+            JSONObject jsonObject = (JSONObject) obj;
+            mailService.setMailHost(jsonObject.getAsString("host"));
+            mailService.setSenderEmailAddress(jsonObject.getAsString("senderEmail"));
+            mailService.setMailUsername(jsonObject.getAsString("mailUsername"));
+            mailService.setMailPassword(jsonObject.getAsString("mailPassword"));
+            mailService.setPortNumber((Integer) jsonObject.get("port"));
+
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        // TODO put these values in a different file , then gitignore file
+
+    }
+
+
+    @Test
+    void shouldSendEmailToNoReply() {
+        mailService.sendEmail("resetlink 2", "noreply.mock.cgi@gmail.com");
+
+    }
+}
