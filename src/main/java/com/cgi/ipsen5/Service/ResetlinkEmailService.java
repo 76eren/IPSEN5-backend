e4546a4a369dc2829ea5ecc3cbb518007cfedb18
isabelhb <isabelbohre@gmail.com>
2024-05-16 12:00:46 +0200

    FEAT: email service can send an email now

--- src/main/java/com/cgi/ipsen5/Service/ResetlinkEmailService.java
+++ src/main/java/com/cgi/ipsen5/Service/ResetlinkEmailService.java
@@ -2,15 +2,27 @@ package com.cgi.ipsen5.Service;
 
 
 import org.simplejavamail.api.email.Email;
+import org.simplejavamail.api.mailer.Mailer;
+import org.simplejavamail.api.mailer.config.TransportStrategy;
 import org.simplejavamail.email.EmailBuilder;
+import org.simplejavamail.mailer.MailerBuilder;
 import org.springframework.beans.factory.annotation.Value;
 
 public class ResetlinkEmailService {
-    @Value("${jwt.spring.mail.username}")
+    @Value("${spring.mail.username}")
     private String senderEmailAddress;
+    @Value("${spring.mail.host}")
+    private String mailHost;
+    @Value("${spring.mail.port}")
+    private int portNumber;
+    @Value("${spring.mail.username}")
+    private String mailUsername;
+    @Value("${spring.mail.password}")
+    private String mailPassword;
+
     private String subject = "Resetlink Werkplekreserverings Applicatie";
 
-    public Email createResetMail(String resetLink, String recipient) {
+    private Email createResetMail(String resetLink, String recipient) {
         return EmailBuilder.startingBlank()
                 .from(senderEmailAddress)
                 .to(recipient)
@@ -18,6 +30,16 @@ public class ResetlinkEmailService {
                 .withPlainText(resetLink)
                 .buildEmail();
 
+    }
+
+    public void sendEmail(String resetLink, String recipient){
+        Email mailToSend = this.createResetMail(resetLink, recipient);
+        Mailer mailer = MailerBuilder
+                .withSMTPServer(mailHost, portNumber, mailUsername, mailPassword)
+                .withTransportStrategy(TransportStrategy.SMTPS)
+                .buildMailer();
+
+        mailer.sendMail(mailToSend);
     }
 
 }
