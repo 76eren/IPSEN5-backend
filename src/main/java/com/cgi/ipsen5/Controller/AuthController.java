e4dc5f44fe52d29d59eaecc5f9e394a01be33e9c
Eren <e.basaran2004@gmail.com>
2024-05-21 15:26:02 +0200

    Fix: fixed login response message not being a json

--- src/main/java/com/cgi/ipsen5/Controller/AuthController.java
+++ src/main/java/com/cgi/ipsen5/Controller/AuthController.java
@@ -1,18 +1,19 @@
 package com.cgi.ipsen5.Controller;
 
+import com.cgi.ipsen5.Dto.Auth.AuthCheckResponseDTO;
 import com.cgi.ipsen5.Dto.Auth.AuthRegisterDTO;
 import com.cgi.ipsen5.Dto.Auth.AuthRequestDTO;
 import com.cgi.ipsen5.Dto.Auth.AuthResponseDTO;
 import com.cgi.ipsen5.Model.ApiResponse;
 import com.cgi.ipsen5.Service.AuthenticationService;
+import jakarta.security.auth.message.AuthStatus;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -40,5 +41,20 @@ public class AuthController {
         String token = tokenResponse.get();
         return new ApiResponse<>(new AuthResponseDTO(token));
     }
+
+    @GetMapping(value = "/authenticated")
+    public ApiResponse<AuthCheckResponseDTO> checkAuthenticated() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        boolean isAuthenticated = authentication != null && authentication.isAuthenticated()
+                && !authentication.getName().equals("anonymousUser");
+
+        AuthCheckResponseDTO authCheckResponseDTO = AuthCheckResponseDTO
+                .builder()
+                .isAuthenticated(isAuthenticated)
+                .build();
+
+        return new ApiResponse<>(authCheckResponseDTO, HttpStatus.OK);
+    }
+
 
 }
