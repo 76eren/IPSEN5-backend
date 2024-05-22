843ffc71b84e32bd9d684179f00f2fb473797435
Stan Smits <s1136644@student.hsleiden.nl>
2024-05-22 10:56:27 +0200

    Revert "fix: remove old dao's"
    This reverts commit c8372b4dec9e549ee1f7bb60aa0694186916ccb4.

---
+++ src/main/java/com/cgi/ipsen5/Dao/ReservationDao.java
@@ -0,0 +1,101 @@
+package com.cgi.ipsen5.Dao;
+
+import com.cgi.ipsen5.Dto.Reservation.ReservationCreateDTO;
+import com.cgi.ipsen5.Model.Location;
+import com.cgi.ipsen5.Model.Reservation;
+import com.cgi.ipsen5.Model.ReservationStatus;
+import com.cgi.ipsen5.Model.User;
+import com.cgi.ipsen5.Repository.ReservationRepository;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class ReservationDao {
+    private final ReservationRepository reservationRepository;
+    private final UserDao userDao;
+    private final LocationDao locationDao;
+    private final ReservationHistoryDao reservationHistoryDAO;
+    private final ReservationDeletionDao reservationDeletionDAO;
+    private final ReservationRepository reservationRepository;
+    private final ReservationHistoryDAO reservationHistoryDAO;
+    private final ReservationDeletionDAO reservationDeletionDAO;
+
+    public ReservationDAO(ReservationRepository reservationRepository
+            ,ReservationHistoryDAO reservationHistoryDAO,
+                          ReservationDeletionDAO reservationDeletionDAO) {
+        this.reservationRepository = reservationRepository;
+        this.reservationHistoryDAO = reservationHistoryDAO;
+        this.reservationDeletionDAO = reservationDeletionDAO;
+    }
+
+        public Reservation save (UUID userId, ReservationCreateDTO reservationCreateDTO){
+            Optional<User> user = this.userDao.findById(userId);
+
+            if (user.isEmpty()) {
+                throw new IllegalArgumentException("User not found");
+            }
+
+            Location location = locationDao.findLocationById(UUID.fromString(reservationCreateDTO.getLocationId()));
+            // TODO: Check if location exists
+
+            Reservation newReservation = Reservation
+                    .builder()
+                    .user(user.get())
+                    .location(location)
+                    .startDateTime(LocalDateTime.parse(reservationCreateDTO.getStartDateTime()))
+                    .endDateTime(LocalDateTime.parse(reservationCreateDTO.getEndDateTime()))
+                    .numberOfPeople(reservationCreateDTO.getNumberOfPeople())
+                    .createdAt(location.getCreatedAt())
+                    .build();
+
+            return this.reservationRepository.save(newReservation);
+        }
+
+        public boolean updateReservationStatus (LocalDateTime start, User userId){
+            Optional<Reservation> optionalReservation = this.reservationRepository
+                    .getByStartDateTimeAndUser(start, userId);
+            if (optionalReservation.isEmpty()) {
+                return false;
+            }
+
+            Reservation reservation = optionalReservation.get();
+            LocalDateTime reservationTime = reservation.getStartDateTime();
+            LocalDateTime allowedLateTime = reservationTime.plusMinutes(15);
+
+            if (start.isBefore(allowedLateTime) && (start.isBefore(reservationTime) || start.isEqual(reservationTime))) {
+                reservation.setStatus(ReservationStatus.CHECKED_IN);
+                this.reservationRepository.save(reservation);
+                return true;
+            }
+
+            return false;
+        }
+
+        public boolean cancelReservation (UUID id){
+            Optional<Reservation> optionalReservation = this.reservationRepository.getById(id);
+            if (optionalReservation.isEmpty()) {
+                return false;
+            }
+
+            Reservation reservation = optionalReservation.get();
+            reservation.setStatus(ReservationStatus.CANCELLED);
+            this.reservationRepository.save(reservation);
+            boolean success = this.reservationHistoryDAO.saveReservationHistory(reservation);
+            boolean deleted = this.reservationDeletionDAO.deleteReservation(id);
+            return success && deleted;
+        }
+
+        public Optional<Reservation> findById (UUID id){
+            return this.reservationRepository.getById(id);
+        }
+
+        public List<Reservation> findAll (UUID userId){
+            // TODO: check if user exists first
+            return this.reservationRepository.findReservationByUserId(userId);
+        }
+    }
