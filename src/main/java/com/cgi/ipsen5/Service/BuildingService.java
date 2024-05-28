af272ff9c7afcef0a33200df260f4de17f93cdc8
Amber de Geus <s1143282@student.hsleiden.nl>
2024-05-28 17:28:45 +0200

    feat: can fetch building by name and throws exception when not found

--- src/main/java/com/cgi/ipsen5/Service/BuildingService.java
+++ src/main/java/com/cgi/ipsen5/Service/BuildingService.java
@@ -1,12 +1,26 @@
 package com.cgi.ipsen5.Service;
 
 import com.cgi.ipsen5.Dao.BuildingDao;
+import com.cgi.ipsen5.Exception.BuildingNotFoundException;
+import com.cgi.ipsen5.Model.Building;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class BuildingService {
     private final BuildingDao buildingDao;
+
+    public Building getBuildingByName(String buildingName) {
+        Optional<Building> optionalBuilding = this.buildingDao.getBuildingByName(buildingName);
+
+        if(optionalBuilding.isEmpty()) {
+            throw new BuildingNotFoundException("Building not found");
+        }
+
+        return optionalBuilding.get();
+    }
 }
