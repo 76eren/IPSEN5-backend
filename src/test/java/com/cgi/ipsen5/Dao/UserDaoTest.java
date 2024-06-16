deafaaecdba4997af880f16ae82b5071bb8ea15f
Eren <e.basaran2004@gmail.com>
2024-06-16 12:51:13 +0200

    Feat: added another test for the standard location

--- src/test/java/com/cgi/ipsen5/Dao/UserDaoTest.java
+++ src/test/java/com/cgi/ipsen5/Dao/UserDaoTest.java
@@ -1,5 +1,6 @@
 package com.cgi.ipsen5.Dao;
 
+import com.cgi.ipsen5.Model.User;
 import com.cgi.ipsen5.Repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,8 +12,9 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class UserDaoTest {
@@ -28,5 +30,18 @@ public class UserDaoTest {
         UUID userId = UUID.randomUUID();
         when(userRepository.findById(userId)).thenReturn(Optional.empty());
         assertThrows(UsernameNotFoundException.class, () -> userDao.getStandardLocation(userId));
+    }
+
+    @Test
+    public void testSetStandardLocation_NullWing() {
+        UUID userId = UUID.randomUUID();
+        User user = new User();
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+
+        userDao.setStandardLocation(userId, null);
+
+        assertNull(user.getStandardLocation());
+        verify(userRepository, times(1)).save(user);
     }
 }
