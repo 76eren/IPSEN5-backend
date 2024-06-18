6b581bef9b6700b2a1311e7f4018a4f4f0ee61a6
Amber de Geus <s1143282@student.hsleiden.nl>
2024-06-18 17:46:43 +0200

    feat: added validation for login and change password

--- src/main/java/com/cgi/ipsen5/Dto/Auth/AuthRequestDTO.java
+++ src/main/java/com/cgi/ipsen5/Dto/Auth/AuthRequestDTO.java
@@ -1,5 +1,9 @@
 package com.cgi.ipsen5.Dto.Auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +14,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthRequestDTO {
+    @NotBlank(message = "Username is required")
+    @Email(message = "Username should be a valid email")
     private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*()\\-_=+\\\\|\\[{\\]};:'\",<.>/?]).{8,}$",
+            message = "Password must have at least one number, " +
+                    "one uppercase letter, " +
+                    "one special character, " +
+                    "and be at least 8 characters long")
     private String password;
 }
--- src/main/java/com/cgi/ipsen5/Seeder/BuildingSeeder.java
+++ src/main/java/com/cgi/ipsen5/Seeder/BuildingSeeder.java
@@ -27,7 +27,7 @@ public class BuildingSeeder {
     public ArrayList<Building> getBuildings() {
         ArrayList<Building> buildings = new ArrayList<>();
         buildings.add(new Building(UUID.randomUUID(),"Amsterdam", "De Entree 21 1101 BH"));
-        buildings.add(new Building(UUID.randomUUID(),"Arhem",  "Utrechtseweg 310 / gebouw B42 6812 AR"));
+        buildings.add(new Building(UUID.randomUUID(),"Arnhem",  "Utrechtseweg 310 / gebouw B42 6812 AR"));
         buildings.add(new Building(UUID.randomUUID(),"Eindhoven",  "DHigh Tech Campus 5 5656 AE"));
         buildings.add(new Building(UUID.randomUUID(),"Groningen",  "Eemsgolaan 1 9727 DW"));
         buildings.add(new Building(UUID.randomUUID(),"Maastricht",  "Stationsplein 12 6221 BT"));
